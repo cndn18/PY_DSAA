6a18aad208250b4b1282e48f5508fa16be502cd9
Chandan Kumar <37476751+cndn18@users.noreply.github.com>
2019-02-13 22:48:18 +0530

    Update depthsearchparenthesis.py

--- depthsearchparenthesis.py
+++ depthsearchparenthesis.py
@@ -1,3 +1,4 @@
+#function to define and count the depth of the parenthesis
 def matched(str):
   count = 0
   max_count=0
@@ -14,6 +15,7 @@ def matched(str):
       return -1
   if(count==0):
       return max_count
+#an example of the function    
 
 f=matched("a)*(?")
 print(f)
